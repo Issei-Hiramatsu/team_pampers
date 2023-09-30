74aa3d681543ce5d9c74d482b84e4c6d122b9a39
issei <issei.hiramatsu@gmail.com>
2023-09-30 18:07:41 +0900

    add right_wrong_body

--- lib/pages/quiz_page/right_wrong_test.dart
+++ lib/pages/quiz_page/right_wrong_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:team_pampers/pages/quiz_page/components/right_wrong_select_button.dart';
+
+import 'components/right_wrong_body.dart';
 
 class RightWrongTestPage extends HookConsumerWidget {
   const RightWrongTestPage({super.key});
@@ -12,28 +13,8 @@ class RightWrongTestPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Column(children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 150),
-              Text("Q1"),
-              SizedBox(height: 40),
-              Text("AかBか"),
-              Spacer(),
-              Row(
-                children: [
-                  RightWrongSelectButton(text: "A"),
-                  RightWrongSelectButton(text: "B"),
-                ],
-              )
-            ],
-          ),
-        ),
-      ]),
+    return const Scaffold(
+      body: RightWrong(),
     );
   }
 }
