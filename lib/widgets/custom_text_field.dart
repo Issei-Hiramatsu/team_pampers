a526d1b3535ee2bc3849f17415cd390973e1e525
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-09-30 14:30:18 +0900

    コンポーネントの修正

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,31 +1,43 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField(
-      {Key? key,
-        this.keybordType,
-        required this.onChanged,
-        required this.controller,
-        required this.labelText,
-        required this.icon,
-        })
-      : super(key: key);
-
-  final void Function(String)? onChanged;
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.keyboardType,
+    required this.labelText,
+    this.textInputAction,
+    this.focusNode,
+    this.obscureText = false,
+    this.onFieldSubmitted,
+    this.autofocus = false,
+    this.icon,
+  });
   final TextEditingController controller;
-  final TextInputType? keybordType;
+  final TextInputType? keyboardType;
+  final FocusNode? focusNode;
+  final bool obscureText;
+  final void Function(String)? onFieldSubmitted;
+  final TextInputAction? textInputAction;
+  final bool autofocus;
   final String labelText;
-  final Icon icon;
+  final IconData? icon;
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-          keyboardType: keybordType,
-          onChanged: onChanged,
-          decoration: InputDecoration(
-            icon: icon,
-            labelText: labelText
-              ),
-        );
+      keyboardType: keyboardType,
+      focusNode: focusNode,
+      onFieldSubmitted: onFieldSubmitted,
+      textInputAction: textInputAction,
+      autofocus: autofocus,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: Theme.of(context).textTheme.bodySmall,
+        icon: Icon(icon),
+      ),
+    );
   }
 }
