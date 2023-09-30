51a6157cba3848753a4c084b82097d2ba3cffc6b
issei <issei.hiramatsu@gmail.com>
2023-09-30 20:34:34 +0900

    update right_wrong_select_buttonをproviderと同期

--- lib/pages/quiz_page/components/right_wrong_select_button.dart
+++ lib/pages/quiz_page/components/right_wrong_select_button.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:team_pampers/pages/quiz_page/hooks/use_test_judge.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:team_pampers/pages/quiz_page/hooks/use_right_wrong_judge.dart';
 
-class RightWrongSelectButton extends StatelessWidget {
+class RightWrongSelectButton extends ConsumerWidget {
   final String text;
   const RightWrongSelectButton({super.key, required this.text});
 
   @override
-  Widget build(BuildContext context) {
-    final didAnswer = useState(false);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final didAnswer = ref.watch(didAnswerProvider);
+    final isCollect = ref.watch(isCollectProvider);
     return Container(
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black),
@@ -18,11 +19,12 @@ class RightWrongSelectButton extends StatelessWidget {
         child: Center(
           //ここに画像を入れる
           child: ElevatedButton(
-            onPressed: didAnswer.value
+            onPressed: didAnswer
                 ? null
                 : () {
-                    testJudge(2, 2) ? print("正解") : print("不正解");
-                    didAnswer.value = true;
+                    ref.read(didAnswerProvider.notifier).state = true;
+                    testJudge(ref, 2, 2);
+                    isCollect ? print("正解") : print("不正解");
                   },
             child: Text(text),
           ),
