18d781c496694d897db1c8cc2822ba75e6999555
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-01 03:00:23 +0900

    5問分のクイズだけQuizPageが表示される

--- lib/pages/quiz_page/components/judge_right_wrong_animation.dart
+++ lib/pages/quiz_page/components/judge_right_wrong_animation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:team_pampers/features/quiz/quiz.dart';
+import 'package:team_pampers/pages/home_page/home_page.dart';
+import 'package:team_pampers/pages/quiz_page/quiz_page.dart';
 import 'package:team_pampers/utils/gen/assets.gen.dart';
 
 class JudgeRightWrongAnimation extends ConsumerWidget {
@@ -14,6 +16,26 @@ class JudgeRightWrongAnimation extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final didAnswer = ref.watch(didAnswerProvider);
     final isCollect = ref.watch(isCorrectProvider);
+    final questionIndex = ref.watch(questionIndexProvider.notifier).state;
+    if (didAnswer) {
+      Future.delayed(const Duration(seconds: 2), () {
+        if (questionIndex == 5) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            HomePage.route(),
+            (_) => false,
+          );
+        } else {
+          ref.watch(questionIndexProvider.notifier).state++;
+          Navigator.pushAndRemoveUntil(
+            context,
+            QuizPage.route(index: questionIndex),
+            (_) => false,
+          );
+        }
+        ref.read(didAnswerProvider.notifier).state = false;
+      });
+    }
 
     return Stack(
       children: [
