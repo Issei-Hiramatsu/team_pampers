eaf1dba7bdc5defe9b07c5f8013d0ceb7f681585
serizawa <86996275+serizawa-T@users.noreply.github.com>
2023-10-01 02:14:48 +0900

    Update home_page.dart

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -5,6 +5,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:team_pampers/pages/profile_page/profile_page.dart';
 import 'package:team_pampers/widgets/custom_button.dart';
 import 'package:team_pampers/widgets/drawer.dart';
+import 'package:team_pampers/pages/home_page/components/home_button.dart';
+import 'package:team_pampers/pages/home_page/components/home_dropdown_button.dart';
+import 'package:team_pampers/utils/utils.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:team_pampers/pages/quiz_page/quiz_page.dart';
 
 class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
@@ -52,11 +57,29 @@ class HomePage extends HookConsumerWidget {
                   ),
                 ],
               ),
-              const SizedBox(height: 8,),
               SizedBox(
-                height: 80,
-                  child: CustomButton(onPressed: (){}, text: 'START')),
-              SizedBox(height: 8,),
+                      height: 80,
+                      child: CustomButton(
+                          onPressed: () {
+                            Navigator.push(context, QuizPage.route());
+                          },
+                          text: 'START'))
+                  .animate(onPlay: (controller) => controller.repeat())
+                  .shimmer(delay: 4000.ms, duration: 1800.ms)
+                  .shake(hz: 4, curve: Curves.easeInOutCubic)
+                  .scale(
+                    begin: const Offset(1, 1),
+                    end: const Offset(1.1, 1.1),
+                    duration: 600.ms,
+                  )
+                  .then(delay: 600.ms)
+                  .scale(
+                    begin: const Offset(1, 1),
+                    end: const Offset(1 / 1.1, 1 / 1.1),
+                  ),
+              const SizedBox(
+                height: 8,
+              ),
               Row(
                 children: [
                   Expanded(
