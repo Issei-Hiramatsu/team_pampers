1e75c1bc70d32c0af9af62f9e30218e23df7eec2
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-10-01 06:36:04 +0900

    リポジトリの作成と追加

---
+++ lib/repositories/file/file_repository_impl.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:team_pampers/repositories/file/file_repository.dart';
+import 'package:team_pampers/utils/utils.dart';
+
+final fileRepositoryImplProvider = Provider<FileRepository>(
+  (ref) => FileRepositoryImpl(
+    ref.watch(authProvider),
+    ref.watch(storageProvider),
+  ),
+);
+
+class FileRepositoryImpl implements FileRepository {
+  FileRepositoryImpl(
+    this._auth,
+    this._storage,
+  );
+  final FirebaseAuth _auth;
+  final FirebaseStorage _storage;
+  static const fileName = 'profile_icon_image.jpg';
+
+  @override
+  User? get currentUser => _auth.currentUser;
+
+  @override
+  Future<File?> pickImageAndUpload() async {
+    final picker = ImagePicker();
+    final uid = currentUser!.uid;
+    final image = await picker.pickImage(source: ImageSource.gallery);
+    final file = File(image!.path);
+    final storageRef = _storage.ref().child('users/$uid/$fileName');
+    await storageRef.putFile(file);
+    return file;
+  }
+
+  @override
+  Future<Image> getDownloadUrl() async {
+    final uid = currentUser!.uid;
+
+    final reference = _storage.ref().child('users/$uid/$fileName');
+    final imageUrl = await reference.getDownloadURL();
+    return Image.network(imageUrl);
+  }
+
+  @override
+  Future<void> deleteFile() async {
+    final uid = currentUser!.uid;
+    final reference = _storage.ref().child('users/$uid/$fileName');
+    await reference.delete();
+  }
+}
