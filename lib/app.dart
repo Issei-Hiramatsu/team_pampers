13e94ad20af8785847b490e00538b41db58c18a1
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-09-30 15:42:37 +0900

    認証状態の維持

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:team_pampers/pages/home_page/home_page.dart';
+import 'package:team_pampers/start_up_page.dart';
 import 'package:team_pampers/widgets/widgets.dart';
 
 class App extends StatelessWidget {
@@ -13,7 +13,7 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const StartUpPage(),
       builder: (BuildContext context, Widget? child) {
         return GestureDetector(
           onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
