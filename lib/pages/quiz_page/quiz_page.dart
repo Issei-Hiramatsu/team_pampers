bfe84ed66574d99545cf0ec56aa44164f543301b
Issei-Hiramatsu <issei.hiramatsu@gmail.com>
2023-09-30 21:51:26 +0900

    Update quiz_page.dart

--- lib/pages/quiz_page/quiz_page.dart
+++ lib/pages/quiz_page/quiz_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'components/quiz_body.dart';
@@ -11,7 +11,6 @@ class QuizPage extends HookConsumerWidget {
       builder: (_) => const QuizPage(),
     );
   }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
